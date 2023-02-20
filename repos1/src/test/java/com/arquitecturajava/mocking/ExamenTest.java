452c543d2446834dd4449c151d9a374a670857e2
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-20 11:57:48 +0100

    mock 2

--- repos1/src/test/java/com/arquitecturajava/mocking/ExamenTest.java
+++ repos1/src/test/java/com/arquitecturajava/mocking/ExamenTest.java
@@ -1,5 +1,6 @@
 package com.arquitecturajava.mocking;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -11,16 +12,32 @@ import org.junit.jupiter.api.Test;
 class ExamenTest {
 
 	@Test
-	void test() {
+	void aprobadotest() {
 		
 		//arrange
 		Nota nota= mock(Nota.class);
+		//configuras
 		when(nota.estaAprobada()).thenReturn(true);
 		
 		
 		Examen examen= new Examen(LocalDate.now(),nota);
 		boolean aprobado=examen.estaAprobado();
 		assertTrue(aprobado);
+		
+	}
+	
+	@Test
+	void suspensotest() {
+		
+		//arrange
+		Nota nota= mock(Nota.class);
+		//configuras
+		when(nota.estaAprobada()).thenReturn(false);
+		
+		
+		Examen examen= new Examen(LocalDate.now(),nota);
+		boolean aprobado=examen.estaAprobado();
+		assertFalse(aprobado);
 		
 	}
 
