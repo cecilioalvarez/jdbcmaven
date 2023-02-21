d75e89058ff43d8810d4bfbfb3c5e1ab451bc2d8
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-21 09:59:19 +0100

    consulta parametrizada

--- repos1/src/main/java/com/arquitecturajava/PruebaJDBCParametrizado.java
+++ repos1/src/main/java/com/arquitecturajava/PruebaJDBCParametrizado.java
@@ -2,28 +2,37 @@ package com.arquitecturajava;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 public class PruebaJDBCParametrizado {
 
 	public static void main(String[] args) {
-		
-		
+
 		ConfiguradorDB c = ConfiguradorDB.getInstance();
-		
-	
+
+		String sql = "select * from Personas where nombre= ?";
 		try (Connection con = DriverManager.getConnection(c.getUrl(), c.getUser(), c.getPassword());
-				Statement s = con.createStatement();
-				ResultSet rs = s.executeQuery(sql)) {
-			
-			
+				PreparedStatement s = con.prepareStatement(sql)) {
+			inicializarParametros(s, "juan");
+			try (ResultSet rs = s.executeQuery()) {
+
+				while (rs.next()) {
+
+					System.out.println(rs.getString("nombre"));
+				}
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		
 
 	}
 
+	private static void inicializarParametros(PreparedStatement p, String campo) throws SQLException {
+		p.setString(1, campo);
+	}
 }
