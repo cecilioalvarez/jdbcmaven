42217df9badb58574171d34d936069b1722a9d75
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-21 11:02:46 +0100

    nuevos

--- repos1/src/main/java/com/arquitecturajava/PruebaJDBCParametrizadoTransaccion.java
+++ repos1/src/main/java/com/arquitecturajava/PruebaJDBCParametrizadoTransaccion.java
@@ -7,41 +7,29 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class PruebaJDBCParametrizado {
+public class PruebaJDBCParametrizadoTransaccion {
 
 	public static void main(String[] args) {
 
 		ConfiguradorDB c = ConfiguradorDB.getInstance();
 
 		String sql = "insert into Personas (nombre,apellidos,edad) values (?,?,?)";
-		try (Connection con = DriverManager.getConnection(c.getUrl(), c.getUser(), c.getPassword())) {
-
-			try (PreparedStatement s = con.prepareStatement(sql)) {
-				con.setAutoCommit(false);
+		try (Connection con = DriverManager.getConnection(c.getUrl(), c.getUser(), c.getPassword());
+				PreparedStatement s = con.prepareStatement(sql)) {
 				s.setString(1, "david");
 				s.setString(2, "sanchez");
 				s.setInt(3, 20);
-
-				s.execute();
-
-				s.setString(1, "gema");
-				s.setString(2, "gomez");
-				s.setInt(3, 30);
-
 				s.execute();
 			
-			} catch (SQLException e) {
-
-				con.rollback();
-
-			}
-			con.commit();
 			
-		} catch (SQLException e1) {
+			
+
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			e.printStackTrace();
 		}
 
+	
 	}
-
+		
 }
