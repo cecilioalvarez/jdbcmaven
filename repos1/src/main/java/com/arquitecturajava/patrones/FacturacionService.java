c02cd80f542c1082dbf8c3b47784bbde3f9af344
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-22 09:48:51 +0100

    nuevos

---
+++ repos1/src/main/java/com/arquitecturajava/patrones/FacturacionService.java
@@ -0,0 +1,53 @@
+package com.arquitecturajava.patrones;
+
+import java.util.List;
+
+public class FacturacionService {
+
+	//interfaces
+	private ClienteRepository repoCliente;
+	private FacturaRepository repoFactura;
+	
+	
+	public FacturacionService(ClienteRepository repoCliente, FacturaRepository repoFactura) {
+		super();
+		this.repoCliente = repoCliente;
+		this.repoFactura = repoFactura;
+	}
+
+
+	public List<Cliente> buscarTodosLosClientes() {
+		return repoCliente.buscarTodos();
+	}
+
+
+	public Cliente buscarUnCliente(int id) {
+		return repoCliente.buscarUno(id);
+	}
+
+
+	public List<Factura> buscarTodasLasFacturas() {
+		return repoFactura.buscarTodos();
+	}
+
+
+	public void insertarFactura(Factura factura) {
+		repoFactura.insertar(factura);
+	}
+
+
+	public void insertarCliente(Cliente cliente) {
+		repoCliente.insertar(cliente);
+	}
+
+
+	public void borrarCliente(Cliente cliente) {
+		repoCliente.borrar(cliente);
+	}
+
+
+	public void actualizarCliente(Cliente cliente) {
+		repoCliente.actualizar(cliente);
+	}
+	
+}
