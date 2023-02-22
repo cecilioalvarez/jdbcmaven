ae204f7d1d7d5a1585d9977e806500ccea435882
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-22 11:02:53 +0100

    nuevos

---
+++ repos1/src/main/java/com/arquitecturajava/complementario/Fechas2.java
@@ -0,0 +1,25 @@
+package com.arquitecturajava.complementario;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Fechas2 {
+
+	public static void main(String[] args) {
+
+		Calendar calendario = Calendar.getInstance();
+		calendario.set(2023, 2, 22);
+
+		DateFormat formateador = new SimpleDateFormat("dd/MM/yyyy");
+
+		System.out.println(formateador.format(calendario.getTime()));
+
+		DateFormat formateador2 = new SimpleDateFormat("dd/MMMM/yy");
+
+		System.out.println(formateador2.format(calendario.getTime()));
+
+	}
+
+}
