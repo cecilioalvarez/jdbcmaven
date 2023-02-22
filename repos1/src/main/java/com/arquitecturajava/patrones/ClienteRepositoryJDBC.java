c97fba8b36d72201424a10e806d3b1c5e0d4c871
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-22 09:30:46 +0100

    duplicar clases

---
+++ repos1/src/main/java/com/arquitecturajava/patrones/ClienteRepositoryJDBC.java
@@ -0,0 +1,58 @@
+package com.arquitecturajava.patrones;
+
+import java.util.List;
+
+import com.arquitecturajava.DataBaseHelper;
+
+public class ClienteRepositoryJDBC implements ClienteRepository {
+
+	@Override
+	public List<Cliente> buscarTodos() {
+
+		String sql = "select * from Clientes";
+		return getHelper().seleccionar(sql, Cliente.class);
+
+	}
+
+	private DataBaseHelper getHelper() {
+		DataBaseHelper helper = new DataBaseHelper();
+		return helper;
+	}
+
+	@Override
+	public Cliente buscarUno(int id) {
+
+		String sql = "select * from Clientes where id ='" + id + "'";
+		return getHelper().seleccionar(sql, Cliente.class).get(0);
+
+	}
+
+	@Override
+	public void insertar(Cliente cliente) {
+
+		String sql = "insert into Clientes  (id,nombre) values ('" +
+		cliente.getId() + "','" + cliente.getNombre()
+				+ "')";
+		getHelper().actualizar(sql);
+
+	}
+
+	@Override
+	public void borrar(Cliente cliente) {
+
+		String sql = "delete from Clientes where id='" + cliente.getId() + "'";
+
+		getHelper().actualizar(sql);
+
+	}
+
+	@Override
+	public void actualizar(Cliente cliente) {
+
+		String sql = "update Cliente set nombre='" + cliente.getNombre() + " where id='" + cliente.getId() + "'";
+
+		getHelper().actualizar(sql);
+
+	}
+
+}
---
+++ repos1/src/main/java/com/arquitecturajava/patrones/Gasto.java
@@ -0,0 +1,26 @@
+package com.arquitecturajava.patrones;
+
+public class Gasto {
+
+	private String concepto;
+	private double importe;
+	public String getConcepto() {
+		return concepto;
+	}
+	public void setConcepto(String concepto) {
+		this.concepto = concepto;
+	}
+	public double getImporte() {
+		return importe;
+	}
+	public void setImporte(double importe) {
+		this.importe = importe;
+	}
+	public Gasto(String concepto, double importe) {
+		super();
+		this.concepto = concepto;
+		this.importe = importe;
+	}
+	
+	
+}
