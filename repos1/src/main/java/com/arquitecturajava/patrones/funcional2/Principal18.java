df50e096f4698014eebdbdcd0d9499bd53a98775
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-23 13:54:20 +0100

    streams

---
+++ repos1/src/main/java/com/arquitecturajava/patrones/funcional2/Principal18.java
@@ -0,0 +1,23 @@
+package com.arquitecturajava.patrones.funcional2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Principal18 {
+
+	enum Level {
+		  LOW,
+		  MEDIUM,
+		  HIGH
+		}
+	
+	public static void main(String[] args) {
+
+		Level nivel= Level.LOW;
+		
+		System.out.println(nivel);
+	}
+}
