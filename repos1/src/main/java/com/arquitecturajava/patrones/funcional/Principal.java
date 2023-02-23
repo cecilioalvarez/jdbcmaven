dd9c15bce6bd9110daee76f27c83cce692216dba
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-23 08:16:46 +0100

    comparadores

---
+++ repos1/src/main/java/com/arquitecturajava/patrones/funcional/Principal.java
@@ -0,0 +1,27 @@
+package com.arquitecturajava.patrones.funcional;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Principal {
+
+	public static void main(String[] args) {
+	
+		
+		Persona p1= new Persona("pedro","gomez",20);
+		Persona p2= new Persona("angel","gomez",20);
+		Persona p3= new Persona("david","gomez",20);
+		List<Persona> lista=Arrays.asList(p1,p2,p3);
+		
+		Collections.sort(lista);
+		
+		for (Persona p: lista) {
+			
+			System.out.println(p.getNombre());
+		}
+		
+
+	}
+
+}
