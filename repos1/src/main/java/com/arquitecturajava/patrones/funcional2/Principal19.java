16dee6e7ee0e2c2c382ddf6c3b86dc72c893e9e7
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-24 08:14:58 +0100

    nuevos

---
+++ repos1/src/main/java/com/arquitecturajava/patrones/funcional2/Principal19.java
@@ -0,0 +1,30 @@
+package com.arquitecturajava.patrones.funcional2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Principal19 {
+
+	public static void main(String[] args) {
+
+		Factura f1 = new Factura("1", "ordenador", 200);
+		Factura f2 = new Factura("2", "tablet", 500);
+		Factura f3 = new Factura("3", "movil", 700);
+
+		//Function<Factura,Double> mifuncion= (f) -> f.getImporte() * 1.21;
+		
+		List<Factura> lista = Arrays.asList(f1, f2, f3);
+		Stream<Factura> flujo = lista.stream();
+		
+		Optional<Factura> oFactura=flujo.filter(f->f.getConcepto().equals("tablet")).findFirst();
+	
+		if(oFactura.isPresent()) {
+			
+			System.out.println(oFactura.get().getConcepto());
+		}
+		
+		
+	}
+}
