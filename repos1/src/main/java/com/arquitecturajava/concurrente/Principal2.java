4e46c0c8d47b42f8accc1d3aa6f69a80712cc794
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-28 09:03:21 +0100

    joins y programacion concurrente

--- repos1/src/main/java/com/arquitecturajava/concurrente/Principal2.java
+++ repos1/src/main/java/com/arquitecturajava/concurrente/Principal2.java
@@ -5,11 +5,20 @@ public class Principal2 {
 	public static void main(String[] args) {
 	
 		
-		
+		// este es hilo principal
 		Thread hilo = new Thread( new TareaNueva("tareaA"));
 		hilo.start();
+		Thread hilo2 = new Thread( new TareaNueva("tareaB"));
+		hilo2.start();
 		
-	
+		try {
+			hilo.join();
+			hilo2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("termino");
 		
 		
 
